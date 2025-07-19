d61b3d88fdd3b32fdf5428ae2bb13450791e2c1f
Maciej Katafiasz <mkatafiasz@nvidia.com>
2025-07-19 14:54:24 -0700

    Add better unit tests
    We now try to open each defined popup, which should catch gross errors
    and incompatibilities

--- tests/test-magit-p4.el
+++ tests/test-magit-p4.el
@@ -1,6 +1,7 @@
 ;;; -*- lexical-binding: t; -*-
 
 (require 'buttercup)
+(require 'cl-lib)
 (require 'magit)
 (require 'magit-p4)
 
@@ -18,3 +19,23 @@
     (magit-status *test-git-repo*)
     (magit-dispatch)
     (expect (buffer-contains-p "4 Git P4" " *transient*"))))
+
+;; This isn't actually exercising these popups, but it should catch the "doesn't even
+;; open" type of errors and incompatibilities that creep in
+(describe "Popup should open without errors:"
+  (before-each
+    (set-up-temp-dir)
+    (set-up-git-repo))
+
+  (after-each
+    (tear-down-temp-dir))
+
+  (cl-loop for popup in '(magit-p4-clone-popup
+                          magit-p4-sync-popup
+                          magit-p4-submit-popup)
+           ;; Ensure variable capture for `popup'
+           do (let ((popup popup))
+                (it (format "%s" popup)
+                  (without-transient-debugger
+                   (funcall popup)
+                   (transient-quit-all))))))
